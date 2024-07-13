5d30db7f2a1586dd763c68cbd17040f5dffcb68b
tructran2003 <truc75001@gmail.com>
2024-07-13 16:50:38 +0700

    sua bai 5 va up bai 6

--- Buổi 1/Bai5.cpp
+++ Buổi 1/Bai5.cpp
@@ -1,24 +1,30 @@
 ﻿#include <stdio.h>
 
 int main() {
-	int a, b, c;
-	float ket_qua;
+    int a, b, c;
+    float ket_qua;
 
-	printf("Nhập số nguyên a: ");
-	scanf_s("%d", &a);
-	printf("Nhập số nguyên b: ");
-	scanf_s("%d", &b);
-	printf("Nhập số nguyên c: ");
-	scanf_s("%d", &c);
+    printf("Nhap so nguyen a: ");
+    scanf_s("%d", &a);
+    printf("Nhap so nguyen b: ");
+    scanf_s("%d", &b);
+    printf("Nhap so nguyen c: ");
+    scanf_s("%d", &c);
 
-	if (c > 0) {
-		ket_qua = (float)c / (a - b);
-		printf("Kết quả của biểu thức c/(a-b) là: %.2f\n", ket_qua);
-	}
-	else {
-		printf("C phải lớn hơn 0\n");
-	}
+    if (c > 0) {
+        if (a - b != 0) {
+            ket_qua = (float)c / (a - b);
+            printf("Kết quả của biểu thức c/(a-b) là: %.2f\n", ket_qua);
+        }
+        else {
+            printf("a - b bằng 0, không thể chia cho 0.\n");
+        }
+    }
+    else {
+        printf("C phải lớn hơn 0\n");
+    }
 
-	return 0;
+    return 0;
 }
+
 
---
+++ Buổi 1/Bài 6.cpp
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char hoTen[100];
+    int ngay, thang, nam;
+    char gioiTinh[10];
+    int tuoi, tuoiNghiHuu, namNghiHuu, thangNghiHuu;
+    int namHienTai = 2024;
+    int tuoiNamNghiHuu = 62;
+    int tuoiNuNghiHuu = 60;
+
+    printf("Nhap ho ten : ");
+    fgets(hoTen, sizeof(hoTen), stdin);
+    hoTen[strcspn(hoTen, "\n")] = 0;  
+
+    printf("Nhap ngay sinh (dd mm yyyy): ");
+    scanf_s("%d %d %d", &ngay, &thang, &nam);
+
+    printf("Nhap gioi tinh (nam/nu): ");
+    scanf_s("%9s", &gioiTinh,sizeof(gioiTinh));
+
+    tuoi = namHienTai - nam;
+
+    // Xử lý ngoại lệ
+    if (tuoi < 18 || (tuoi > 60 && strcmp(gioiTinh, "nu") == 0) || (tuoi > 62 && strcmp(gioiTinh, "nam") == 0)) {
+        printf("Loi: Tuoi khong thuoc trong tuoi lao dong. Ma loi: 101\n");
+        return 101;
+    }
+
+    if (strcmp(gioiTinh, "nam") != 0 && strcmp(gioiTinh, "nu") != 0) {
+        printf("Loi: Gioi tinh khong hop le. Ma loi: errcode\n");
+        return -1;
+    }
+
+    
+    if (strcmp(gioiTinh, "nam") == 0) {
+        tuoiNghiHuu = tuoiNamNghiHuu;
+    }
+    else {
+        tuoiNghiHuu = tuoiNuNghiHuu;
+    }
+
+    namNghiHuu = nam + tuoiNghiHuu;
+    thangNghiHuu = thang;
+
+    printf("%s, gioi tinh %s, sinh ngay %02d/%02d/%d. Hien tai (nam %d) đa %d tuoi. Thoi gian duoc nghi huu la: %02d/%d.\n",
+        hoTen, gioiTinh, ngay, thang, nam, namHienTai, tuoi, thangNghiHuu, namNghiHuu);
+
+    return 0;
+}
