8b0d3ac7df29ed93d693fddac40bf33b595b30d4
tructran2003 <truc75001@gmail.com>
2024-07-13 14:40:05 +0700

    Tao lai bo cuc git

---
+++ Buổi 1/Bai5.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	float ket_qua;
+
+	printf("Nhập số nguyên a: ");
+	scanf_s("%d", &a);
+	printf("Nhập số nguyên b: ");
+	scanf_s("%d", &b);
+	printf("Nhập số nguyên c: ");
+	scanf_s("%d", &c);
+
+	if (c > 0) {
+		ket_qua = (float)c / (a - b);
+		printf("Kết quả của biểu thức c/(a-b) là: %.2f\n", ket_qua);
+	}
+	else {
+		printf("C phải lớn hơn 0\n");
+	}
+
+	return 0;
+}
+
